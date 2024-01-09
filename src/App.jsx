cb55779e7d5fc05598e8dc97e175b4c9f7b3bed5
Charles Owusu <charlesowusu274@gmail.com>
2024-01-09 10:52:27 +0000

    Remove  the router from the main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { ToDo } from "./components/To-do";
 import "./components/styles/ListItem.css";
 import "./components/styles/App.css";
-import Form from "./components/Form";
 
 export default function App() {
   return (
     <div className="container">
       <ToDo />
-      <Form />
     </div>
   );
 }
